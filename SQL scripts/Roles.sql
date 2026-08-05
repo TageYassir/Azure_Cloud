dbbcbf6ea9ae33b516a6ad66970a1c5c0a8f16df
Tagemouati Yassir <yassir.tagemouati@eidia.ueuromed.org>
2026-08-05 14:06:07 +0100

    roles update

--- SQL scripts/Roles.sql
+++ SQL scripts/Roles.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE supplychain_db;
 
 use supplychain_db;
+use gold_db;
 
 CREATE USER [adf-supplychain-1] FROM EXTERNAL PROVIDER;
 
@@ -10,5 +11,7 @@ ALTER ROLE db_datareader ADD MEMBER [adf-supplychain-1];
 CREATE USER [purview-supplychain-prod] FROM EXTERNAL PROVIDER;
 ALTER ROLE db_datareader ADD MEMBER [purview-supplychain-prod];
 
+-- Octroyer la permission d'opération en bloc au niveau de la base de données ou du serveur via un rôle approprié
+GRANT ADMINISTER DATABASE BULK OPERATIONS TO [purview-supplychain-prod];
 
 SELECT name FROM sys.database_principals WHERE name = 'adf-supplychain-1';
